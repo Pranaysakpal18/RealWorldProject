9521d9f6f6b68dbd8930c1659fad17238d2d0b5a
Pranaysakpal18 <pranay.sakpal18@gmail.com>
2025-07-27 20:58:08 +0530

    filter created

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { FaTasks } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { TbReportSearch } from "react-icons/tb";
+import { FaArrowLeft } from "react-icons/fa";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const DashBoard = () => {
 
@@ -18,6 +20,11 @@ const DashBoard = () => {
   const [user,setUSer]=useState([])
   const [loading,setLoading]=useState(true)
   const [error,setError]=useState('')
+  const [currentPage , setCurrentPage] = useState(1);
+  const [searchTerm , setSearchTerm] =useState('')
+
+
+  const usersPerPage =5;
 
   useEffect(()=>{
     setLoading(true)
@@ -37,6 +44,29 @@ const DashBoard = () => {
     }
     fetchUsers()
   },[])
+
+
+
+
+
+  //Filter user By search
+
+  const filterUser = user.filter(user=>
+    user.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())  ||
+    user.email.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+    
+  )
+
+
+
+
+  //Pagination Logic 
+  const indexOfLastUser =currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+
+  const currentUSers = filterUser.slice(indexOfFirstUser,indexOfLastUser);
+
+  const totalPages = Math.ceil(filterUser.length / usersPerPage)
 
 
 
@@ -63,6 +93,25 @@ const DashBoard = () => {
       
     </div>
 
+
+  
+  {/* search input */}
+
+
+    <div className='mb-6 mt-10 text-center'>
+      <input type="text" placeholder='Search By Name or Email...' value={searchTerm} 
+      onChange={e => setSearchTerm(e.target.value)}
+      className='w-full md:w-1/2 px-4 py-2 border rounded shadow-sm'
+      
+      />
+    </div>
+
+
+
+
+
+
+
     <div className='text-center mt-10'>
       <h2 className='text-xl font-semibold mb-4'>User List</h2>
 
@@ -71,9 +120,11 @@ const DashBoard = () => {
         <p className='text-blue-500'>Loading Users</p>
       ) : error ? (
         <p className='text-red-500'>{error}</p>
-      ) : (
+      ) : filterUser.length === 0 ?(
+        <p className='text-red-500'>No User Found.</p>
+      ) :(
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-          {user.map((items)=>{
+          {currentUSers.map((items)=>{
             return(
               <div className='bg-white p-4 rounded shadow' key={items.id}>
                 <p className='font-bold text-lg'>{items.name}</p>
@@ -81,16 +132,41 @@ const DashBoard = () => {
                 <p className='text-sm text-gray-500'>{items.address.city}</p>
 
               </div>
+              
             )
 
           })}
         </div>
+        
+
+        
+
+
+
+
+
       )
 
 
 
 
      }
+
+
+
+
+    </div>
+
+    <div className='flex justify-between items-center mt-4'>
+      <button className='px-4 py-2 bg-gray-300 rounded disabled:opacity-50' onClick={()=>setCurrentPage(currentPage -1)} disabled={currentPage ===1}><FaArrowLeft/> Prev</button>
+
+      <p>Page {currentPage} of {totalPages}</p>
+
+      <button className='px-4 py-2 bg-gray-300 rounded disabled:opacity-50' onClick={()=>setCurrentPage(currentPage+1)}
+        disabled={currentPage === totalPages}>
+           <FaLongArrowAltRight/> Next
+
+      </button>
 
 
 
